16893cf901662d04a01c821f94f382ed201f8970
wnmk216 <wanthanee.p@itm.kmutnb.ac.th>
2026-04-24 19:06:54 +0700

    Add files via upload

---
+++ DeployModel/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+st.set_page_config(page_title="Boot Camp DS & ML", layout="wide")
+
+st.sidebar.title("App Menu")
+
+ 
+st.title("🏠 หน้าหลัก")
+st.header("Boot Camp: Data Science and Machine Learning")
+st.info("7 Day Intensive Hands-on Workshop")
+
+
+
+
