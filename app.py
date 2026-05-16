a3861daecc764ef836d0b8c01131fb418f616286
wnmk216 <wanthanee.p@itm.kmutnb.ac.th>
2026-05-16 15:38:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,14 @@ elif st.button("การแปลงข้อมูล"):
     st.switch_page("pages/transform_app.py")
 elif st.button("การวิเคราะห์ข้อมูลเชิงสำรวจ"):
     st.switch_page("pages/EDA_app.py")
+elif st.button("การพยากรณ์ยอดขายแบบง่าย"):
+    st.switch_page("pages/sale_predict.py")
+elif st.button("การพยากรณ์ระยะเวลาการให้บริการขนส่ง"):
+    st.switch_page("pages/sale_predict.py")
+elif st.button("การจำแนกประเภทข้อมูลยอดขาย"):
+    st.switch_page("pages/classify_redbull_sale.py")
+elif st.button("การจัดกลุ่มข้อมูล"):
+    st.switch_page("clustering_segment.py")
+elif st.button("ระบบแนะนำ"):
+    st.switch_page("association_recommend.py")
 
