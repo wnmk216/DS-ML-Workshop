183e0d44512e288c4595d39be977e0604cb3d281
wnmk216 <wanthanee.p@itm.kmutnb.ac.th>
2026-05-16 13:24:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
-st.set_page_config(page_title="Boot Camp DS & ML", layout="wide")
+st.set_page_config(page_title="MyApp", layout="wide")
 
-st.sidebar.title("App Menu")
-
- 
-st.title("🏠 หน้าหลัก")
-st.header("Boot Camp: Data Science and Machine Learning")
+st.title("🏠 หน้าหลัก ")
+st.write("### Boot Camp: Data Science and Machine Learning")
 st.info("7 Day Intensive Hands-on Workshop")
 
 
-
+if st.button("การทำความสะอาดข้อมูล"):
+    st.switch_page("pages/cleaning_app.py")
+elif st.button("การแปลงข้อมูล"):
+    st.switch_page("pages/transform_app.py")
 
