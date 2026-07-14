25303b16a1694e84d2bac4186af48e17620d4c7a
wnmk216 <wanthanee.p@itm.kmutnb.ac.th>
2026-07-14 12:09:17 +0700

    Update coconut.py

--- pages/coconut.py
+++ pages/coconut.py
@@ -7,12 +7,6 @@ import numpy as np
 import tempfile
 import os
 
-# Install opencv-python to ensure it's available in the Streamlit environment
-# This addresses ImportError related to cv2
-if 'opencv-python' not in st.session_state:
-    st.session_state['opencv-python'] = True
-    st.spinner('Installing OpenCV... please wait')
-    os.system('pip install opencv-python -q')
 
 # Set page configuration
 st.set_page_config(
